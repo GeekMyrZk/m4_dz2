c329051564156a59469b184f60eed53b5d0c6772
GeekMyrZk <dlaprogramminga@gmail.com>
2024-01-26 20:03:11 +0600

    dz2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 
 function App() {
-  return (
+    const [color, setColor] = useState('blue')
+    const changeColor = ()=> setColor(color => color === 'blue' ? 'red' : 'blue')
+    return (
       <div className="App">
         <>
-            div
-
-
-
+            <div onClick={changeColor}
+                style={{
+                    backgroundColor: color,
+                    margin: '0 auto 10px auto',
+                    width: '100px', height: '100px',
+                    border: 'solid grey 1px'}}
+            ></div>
+            <div onClick={changeColor}
+                style={{
+                    backgroundColor: color === 'blue' ? 'red' : "blue",
+                    margin: '0 auto',
+                    width: '100px', height: '100px',
+                    border: 'solid grey 1px'}}
+            ></div>
         </>
       </div>
-  );
+    );
 }
 
 export default App;
